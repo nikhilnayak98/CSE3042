e10facdc93cdb142852667180af32f6f0207cd5d
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-09-25 12:45:04 +0530

    prog

--- Chapter6/src/ProgrammingProjects/Prog2.java
+++ Chapter6/src/ProgrammingProjects/Prog2.java
@@ -1,18 +1,41 @@
 /*
  * Name: Nikhil Ranjan Nayak
  * Regd No: 1641012040
- * Desc: Binary tree
+ * Desc: Knapsack
  */
 package ProgrammingProjects;
 
 public class Prog2 {
 
+	public static double[] sack;
+
 	public static void main(String[] args) {
-
-	}
-	
-	public static void knapsack() {
-		
+		sack = new double[]{11, 8, 7, 6, 5};
+		if(!knapsack(20,0))
+			System.out.println("No Combinations");		
 	}
 
+	public static boolean knapsack(double targetWeight, int index) {
+		boolean complete = false;	
+		if(index == sack.length) 
+			return false;
+
+		if(sack[index] == targetWeight) {
+			System.out.print("Answer: " + sack[index] + " ");
+			complete = true;
+		}
+
+		if(sack[index] < targetWeight) {
+			complete = knapsack(targetWeight - sack[index], index + 1);
+			if(complete) 
+				System.out.print(sack[index] + " ");
+			for(int i = index + 1; i < sack.length; i++) {
+				if(!complete) 
+					complete = knapsack(targetWeight, i);
+			}
+		}
+		if(sack[index] > targetWeight)
+			complete = knapsack(targetWeight, index + 1);
+		return complete;
+	}
 }
